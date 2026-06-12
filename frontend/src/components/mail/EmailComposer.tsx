7531e36f6c3eb18b14e2fa3dd7bfef10d86650ef
YasirJafar123 <bhattiyasir349@gmail.com>
2026-06-12 07:09:24 -0700

    updates

--- frontend/src/components/mail/EmailComposer.tsx
+++ frontend/src/components/mail/EmailComposer.tsx
@@ -284,6 +284,7 @@ export function EmailComposer({ open, onOpenChange, mailboxes, replyTo, forwardE
             />
           </div>
 
+
           <div className="space-y-1 mb-12">
             <ReactQuill
               theme="snow"
